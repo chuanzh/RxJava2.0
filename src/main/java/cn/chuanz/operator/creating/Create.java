2f556477135c40fdfd6bc92d97cef0f80e78a5e4
张川 <zhangchuan@zhangchuandeMacBook-Pro.local>
2017-11-28 19:39:54 +0800

    observable-observer

--- src/main/java/cn/chuanz/operator/creating/Create.java
+++ src/main/java/cn/chuanz/operator/creating/Create.java
@@ -1,14 +1,15 @@
 package cn.chuanz.operator.creating;
 
-import java.util.Random;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.subjects.Subject;
 
 
 public class Create {
@@ -16,7 +17,7 @@ public class Create {
 	private Logger logger = LoggerFactory.getLogger(Create.class);
 	
 	public static void main(String[] args) {
-		new Create().create();
+		new Create().create2();
 	}
 	
 	
@@ -24,25 +25,187 @@ public class Create {
 	 * 创建函数
 	 */
 	public void create() {
-		Observable.create(new ObservableOnSubscribe<String>() {
+		//被观察者
+		Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
 			@Override
 			public void subscribe(ObservableEmitter<String> t) throws Exception {
 				t.onNext("hello");
-				if (new Random().nextInt(10) > 5) {
-					t.onError(new Exception());
-				}
+				
+				t.onNext("word");
+				
+				//t.onError(new Exception());
+				
 				t.onComplete();
-			}
-		})
-		.subscribe(new Consumer<String>() {
-			@Override
-			public void accept(String arg0) throws Exception {
-				// TODO Auto-generated method stub
-				logger.info(arg0);
+				
+				t.onNext("new str");
 			}
 		});
 		
 		
+		Subject<String> subject = new Subject<String>() {
+
+			@Override
+			public void onSubscribe(Disposable d) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onNext(String value) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onError(Throwable e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onComplete() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public boolean hasObservers() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public boolean hasThrowable() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public boolean hasComplete() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public Throwable getThrowable() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			protected void subscribeActual(Observer<? super String> observer) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		
+		
+		//观察者
+		Observer<String> observer = new Observer<String>() {
+			@Override
+			public void onSubscribe(Disposable d) {
+				logger.info("disposable: "+d.isDisposed());
+			}
+
+			@Override
+			public void onNext(String value) {
+				logger.info(value);
+			}
+
+			@Override
+			public void onError(Throwable e) {
+				logger.info("Error: {}", e);
+			}
+
+			@Override
+			public void onComplete() {
+				logger.info("oncomplete");
+			}
+		};
+		
+		observable.subscribe(observer);
+		
+		observable.subscribe(subject);
+		
 	}
+	
+	
+	private void create2() {
+		
+		Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+			@Override
+			public void subscribe(ObservableEmitter<String> t) throws Exception {
+				t.onNext("hello");
+				
+				t.onNext("word");
+				
+				//t.onError(new Exception());
+				
+				t.onComplete();
+				
+				t.onNext("new str");
+			}
+		});
+		
+		Subject<String> subject = new Subject<String>() {
+
+			@Override
+			public void onSubscribe(Disposable d) {
+			}
+
+			@Override
+			public void onNext(String value) {
+				logger.info(value);
+			}
+
+			@Override
+			public void onError(Throwable e) {
+				
+			}
+
+			@Override
+			public void onComplete() {
+				
+			}
+
+			@Override
+			public boolean hasObservers() {
+				return false;
+			}
+
+			@Override
+			public boolean hasThrowable() {
+				return false;
+			}
+
+			@Override
+			public boolean hasComplete() {
+				return false;
+			}
+
+			@Override
+			public Throwable getThrowable() {
+				return null;
+			}
+
+			@Override
+			protected void subscribeActual(Observer<? super String> observer) {
+				
+			}
+		};
+		
+		observable.subscribe(subject);
+		
+		subject.subscribe(new Consumer<String>() {
+
+			@Override
+			public void accept(String t) throws Exception {
+				// TODO Auto-generated method stub
+				logger.info(t);
+			}
+		});
+		
+	}
+	
 	
 }
